b0e60cc75dec5b310fc235f761175450f6b02c8f
Neven Hajder <neven.hajder@gmail.com>
2019-07-16 05:39:44 -0400

    Destructuring Objects

---
+++ es6.js
@@ -0,0 +1,18 @@
+const data = {
+    w: '500px',
+    h: '250px',
+    x: '20px',
+    y: '20px'
+};
+
+const {
+    w: width,
+    h: height,
+    x: posX,
+    y: posY
+} = data;
+
+console.log(width);
+console.log(height);
+console.log(posX);
+console.log(posY);
