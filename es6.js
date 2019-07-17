4e4f91bb06c48e813cc0bc6fec3e2f15f467aebd
Neven Hajder <neven.hajder@gmail.com>
2019-07-17 06:07:48 -0400

    Looping over Node List (For of loop).

--- es6.js
+++ es6.js
@@ -1,21 +1,25 @@
-function convertCurrency(amount) {
-    const converted = {
-        USD: amount * 0.76,
-        GBP: amount * 0.53,
-        AUD: amount * 1.01,
-        MEX: amount * 13.30
+const listItems = document.querySelectorAll('li');
+
+// Iterate over Node List using a traditional for loop
+// for (var i=0; i < listItems.length; i++) {
+//     listItems[i].textContent = `list item ${i + 1}`;
+// }
+
+
+// Node List has the forEach method!
+// listItems.forEach((el, i) => el.textContent = `list item ${i + 1}`);
+
+
+// Iterates over the prototype methods and properties!!!
+// for (const index in listItems) {
+//     console.log(index);
+// }
+
+
+// Can use break or continue
+for (const [index, item] of listItems.entries()) {
+    if (index === 2) {
+        continue;
     }
-
-    return converted;
+    item.textContent = `list item ${index + 1}`;
 }
-
-const {USD, GBP, AUD, MEX} = convertCurrency(100);
-console.log(USD, GBP);
-console.log('-----');
-
-function tipCalc({ total, tip = 0.15, tax = 0.13 }) {
-    return total + (total * tip) + (total * tax);
-};
-
-const bill = tipCalc({ total: 100 });
-console.log(bill);
