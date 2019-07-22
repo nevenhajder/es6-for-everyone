cceb0b45db26b3683464c12c5ba6d0f6c681d5af
Neven Hajder <neven.hajder@gmail.com>
2019-07-22 06:31:36 -0400

    One promise.

--- es6.js
+++ es6.js
@@ -1,54 +1,29 @@
-const first = 'snickers';
-const last = 'bos';
-const age = 2;
-const breed = 'King Charles Cav';
+const posts = [
+    { title: 'I love JavaScript', author: 'Wes Bos', id: 1 },
+    { title: 'CSS!', author: 'Chris Coyier', id: 2 },
+    { title: 'Dev tools tricks', author: 'Addy Osmani', id: 3 },
+];
 
-const dog = {
-    // No longer need this repetition
-    first: first,
-    // When the object property is the same as
-    // a variable, it will automatically be set
-    last,
-    age,
-    breed
-};
-console.log(dog);
+const authors = [
+    { name: 'Wes Bos', twitter: '@wesbos', bio: 'Canadian Developer' },
+    { name: 'Chris Coyier', twitter: '@chriscoyier', bio: 'CSS Tricks and CodePen' },
+    { name: 'Addy Osmani', twitter: '@addyosmani', bio: 'Googler' },
+];
 
-const modal = {
-    // This can be written more simply now
-    create: function() {
-
-    },
-    // Shorthand for setting methods
-    open() {
-
-    },
-    close() {
-
-    }
+function getPostById(id) {
+    return new Promise((resolve, reject) => {
+        // mimick database with timeout
+        setTimeout(() => {
+            const foundPost = posts.find(post => post.id === id);
+            if (foundPost) {
+                resolve(foundPost);
+            } else {
+                reject(Error);
+            }
+        }, 1000);
+    });
 }
 
-
-// Computed Property Names
-const key = 'pocketColor';
-const value = '#ffc600';
-const oppositeValue = '#0039ff';
-
-const tShirt = {
-    [key]: value,
-    // Can compute property names
-    [`${key}Opposite`]: oppositeValue
-};
-console.log(tShirt);
-
-
-const keys = ['size', 'color', 'weight'];
-const values = ['medium', 'red', 100];
-
-const shirt = {
-    [keys.shift()]: values.shift(),
-    [keys.shift()]: values.shift(),
-    [keys.shift()]: values.shift()
-};
-
-console.log(shirt);
+getPostById(2)
+    .then(post => console.log(post))
+    .catch(() => console.log('No post found.'));
