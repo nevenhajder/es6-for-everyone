141cd407406e1d2d2f9b5a0054299b65dcac9ecf
Neven Hajder <neven.hajder@gmail.com>
2019-07-25 06:52:04 -0400

    Added Babel for IE11 support.

--- es6.js
+++ es6.js
@@ -1,32 +1,5 @@
-// Example simulating API calls
-const weather = new Promise((resolve) => {
-    setTimeout(() => {
-        resolve({ temp: 29, weather: 'Sunny with clouds' });
-    }, 2000);
-});
+const theArray = [1, 2, 3, 4, 5];
 
-const tweets = new Promise((resolve) => {
-    setTimeout(() => {
-        resolve(['I like cake.', 'BBQ is good!']);
-    }, 500);
-});
+const [one, two, three, four, five] = theArray;
 
-Promise
-    .all([weather, tweets])
-    .then((responses) => {
-        const [weatherInfo, tweetInfo] = responses;
-        // console.log(weatherInfo, tweetInfo);
-    });
-
-
-// Example with API calls
-const postsPromise = fetch('http://wesbos.com/wp-json/wp/v2/posts');
-const streetCarsPromise = fetch('http://data.ratp.fr/api/datasets/1.0/search/?q=paris');
-
-Promise
-    .all([postsPromise, streetCarsPromise])
-    .then((responses) => {
-        // At this point responses are a readable stream that needs to be converted to JSON
-        return Promise.all(responses.map(response => response.json()));
-    })
-    .then(responses => console.log(responses));
+console.log(two);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,22 @@
 module.exports = {
     mode: process.env.NODE_ENV || 'production',
     devtool: 'source-map',
-    entry: './app.js',
+    entry: './es6.js',
     output: {
         filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
     },
 };
