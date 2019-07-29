cd40115f682ab311ef7633feb0b3be4b7237c521
Neven Hajder <neven.hajder@gmail.com>
2019-07-29 06:02:38 -0400

    Basics of Classes

--- es6.js
+++ es6.js
@@ -1,5 +1,25 @@
-const theArray = [1, 2, 3, 4, 5];
+class Dog {
+    constructor(name, breed) {
+        this.name = name;
+        this.breed = breed;
+    }
 
-const [one, two, three, four, five] = theArray;
+    bark() {
+        console.log(`Bark! Bark! My name is ${this.name}`);
+    }
 
-console.log(two);
+    cuddle() {
+        console.log('I love you owner!');
+    }
+
+    set nicknames(name) {
+        this.nick = name.trim();
+    }
+
+    get nicknames() {
+        return this.nick;
+    }
+}
+
+const Snickers = new Dog('Snickers', 'King Charles');
+console.log(Snickers);
