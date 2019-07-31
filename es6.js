4bd55c260a26131f5f813f2443cef3b632b232a7
Neven Hajder <neven.hajder@gmail.com>
2019-07-31 06:04:20 -0400

    Sets and weak sets.

--- es6.js
+++ es6.js
@@ -1,16 +1,34 @@
-// A generator function yields a new value each time it's called
-// until it has reached the final yield statement.
-function* generatorExample() {
-    yield '1';
-    yield '2';
-    yield '3';
+const brunch = new Set();
+
+brunch.add('Wes');
+brunch.add('Sarah');
+brunch.add('Simone');
+
+const line = brunch.values();
+// console.log(line.next().value);
+// console.log(line.next().value);
+
+// You can iterate over sets
+// but not over weak sets
+for (const person of brunch) {
+    console.log(person);
 }
 
-const count = generatorExample();
+brunch.add('Snickers');
+brunch.add('Heather');
 
-// Calling .next() on a generator function returns
-// an object with the properties 'done' and 'value'.
-console.log(count.next());
-console.log(count.next());
-console.log(count.next());
-console.log(count.next());
+// ----------------------------------------------------------------------------
+
+// Weak sets (weakly held sets) can only contain objects
+const dog1 = { name: 'Snickers', age: 3 };
+let dog2 = { name: 'Sunny', age: 1 };
+
+const weakness = new WeakSet([dog1, dog2]);
+
+console.log(weakness);
+dog2 = null;
+console.log(weakness);
+// At some point between the console.log right above and this timeout log
+// the reference to dog2 is garbage collected from the weak set
+// since dog2 has been manually set to null. This is what is meant by "weakly held sets"
+setTimeout(() => console.log(weakness), 4000);
